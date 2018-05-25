4f017f774480463f1cc9b25458348c9be52bf536
Laura Turk <laura.turk@cognizant.com>
2018-05-25 15:27:11 -0600

    ch 4 d + c in python

--- python/test.py
+++ python/test.py
@@ -6,7 +6,27 @@ def countDownRecursion(i, store):
         countDownRecursion(i - 1, store)
     return store
 
+def sumFunction(list):
+    if list == []:
+        return 0
+    return list[0] + sumFunction(list[1:])
+
+def countList(list):
+    if list == []:
+        return 0
+    return 1 + countList(list[1:])
+
 def test_countDownRecursion():
     testList = []
     assert countDownRecursion(4, testList) == [4, 3, 2, 1, 0]
+
+def test_sumFunction():
+    assert sumFunction([1, 2, 3, 4, 5 ]) == 15
+    assert sumFunction([1, 2]) == 3
+    assert sumFunction([]) == 0
+
+def test_countList():
+    assert countList([1, 2, 3, 4, 5]) == 5
+    assert countList([]) == 0
+    assert countList([1, 2]) == 2
 
